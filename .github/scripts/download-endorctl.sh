e75e1e055ed0425804fe7b1b3425ce2a5d96b371
Jon Jarboe <jrjarboe@gmail.com>
2023-07-20 14:34:53 -0700

    add SBOM/VEX steps

---
+++ .github/scripts/download-endorctl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "${ENDORCTL_VERSION:-latest}" == "latest" ]; then
+   echo "Downloading latest version of endorctl";
+   ENDORCTL_SHA=$(curl --silent https://api.endorlabs.com/meta/version | jq -r '.ClientChecksums.ARCH_TYPE_LINUX_AMD64');
+   VERSION=$(curl --silent https://api.endorlabs.com/meta/version | jq -r '.Service.Version');
+   curl --silent https://storage.googleapis.com/endorlabs/"$VERSION"/binaries/endorctl_"$VERSION"_linux_amd64 -o endorctl;
+   echo "$ENDORCTL_SHA  endorctl" | sha256sum -c;
+   if [ $? -ne 0 ]; then 
+      echo "Integrity check failed"; 
+      exit 1;
+   fi
+else
+   echo Downloading version "$ENDORCTL_VERSION" of endorctl;
+   if [ "$ENDORCTL_SHA" = "" ]; then echo "WARNING: ENDORCTL_SHA not set"; fi
+   curl --silent https://storage.googleapis.com/endorlabs/"$ENDORCTL_VERSION"/binaries/endorctl_"$ENDORCTL_VERSION"_linux_amd64 -o endorctl;
+   echo "$ENDORCTL_SHA  endorctl" | sha256sum -c;
+   if [ $? -ne 0 ]; then 
+      echo "Integrity check failed for the pinned version of endorctl. Please ensure the environment variable is set and re-run the job"; 
+      exit 1;
+   fi
+fi
+chmod +x ./endorctl
